05b6711bff4e20482d65de70eabd728afae8b8af
Edward P Cabusora <ecabu001@ucr.edu>
2020-06-01 21:30:49 -0700

    Update site.js
    deleted function, adjusted button function

--- js/site.js
+++ js/site.js
@@ -1,11 +1,6 @@
 // Hide side navbar with toggle button
-$(".hide-btn").on("click", function(){
+$(".toggle-btn").on("click", function(){
   $("#sidebar-wrapper").hide();
-});
-
-//Show side navbar
-$(".show-btn").on("click", function(){
-  $("#sidebar-wrapper").show();
 });
 
 //Print user input in console.console console log
